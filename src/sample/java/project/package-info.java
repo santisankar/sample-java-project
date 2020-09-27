424453affdb0bab3a96f3c60cec9f7fb1386e2e9
Santi C <schowdhury.ju@gmail.com>
2020-09-27 10:24:31 +0530

    Sample message for demostrating the SSH communication

--- src/sample/java/project/package-info.java
+++ src/sample/java/project/package-info.java
@@ -5,4 +5,4 @@ package sample.java.project;
 
 // This has been added to check the pull comments
 // This has been added to check the push comments
-
+// This has been added to check the SSH, not Https protocol
