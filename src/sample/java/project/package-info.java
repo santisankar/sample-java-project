8804c7863a91f2392e2755fb6240832c15d9d5b0
Santi C <schowdhury.ju@gmail.com>
2020-09-27 16:31:11 +0530

    This commit is for SSH communication with password

--- src/sample/java/project/package-info.java
+++ src/sample/java/project/package-info.java
@@ -6,3 +6,4 @@ package sample.java.project;
 // This has been added to check the pull comments
 // This has been added to check the push comments
 // This has been added to check the SSH, not Https protocol
+// SSH Communication based on password
